57fe8d68d6112c55774e819191df7a85f800370e
kaziashfaq <40367281+kaziashfaq@users.noreply.github.com>
2021-12-21 23:52:51 -0700

    Madlib code

---
+++ madlib.py
@@ -0,0 +1,17 @@
+# Author: Kazi Ashfaq
+## Creating a madlib program for fun
+
+noun1 = input("Enter a noun: ")
+noun2 = input("Enter another noun: ")
+noun3 = input("Enter another noun: ")
+adj1 = input("Enter an adjective: ")
+adj2 = input("Enter another adjective: ")
+verb1 = input("Enter a verb: ")
+verb2 = input("Enter another verb: ")
+verb3 = input("Enter another verb: ")
+
+madlib = f"My name is {noun1} and I like to play {noun2}. Its been a long time since I played {noun3} last. \
+I used to be very {adj1} and {adj2} but now, may be not so much. I also like to {verb1} and {verb2} but due to lack \
+of time, I dont really do them much anymore. Good bye for now, I will have to {verb3}"
+
+print(madlib)
